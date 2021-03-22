9ef1155504d35bb5c05651f25850b36d08667f18
Pratik Sawant <pratiksawant22619@gmail.com>
2021-03-22 21:31:23 +0530

    initial commit

--- app.js
+++ app.js
@@ -140,9 +140,9 @@ app.get("/about",function(req,res){
 
 let port = process.env.PORT;
 if (port == null || port == "") {
-  port = 8000;
+  port = 3000;
 }
 
-app.listen(3000,function(){
+app.listen(port,function(){
     console.log("Server Has Started Successfully....");
 })
